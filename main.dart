bf4547bd98580ef7a6491d87b32a4dc2752b3a04
Jannatul Mahia <jannatulmahia8215@gmail.com>
2026-06-13 00:33:00 +0600

    learnt Variables and Data Types

--- main.dart
+++ main.dart
@@ -1,11 +1,53 @@
-void main() {
-  String name = "Mahia";
-  int age = 22;
-  double cgpa = 3.80;
-  bool isStudent = true;
+// void main() {
+//   String name = "Mahia";
+//   int age = 22;
+//   double cgpa = 3.80;
+//   bool isStudent = true;
 
-  print(name);
-  print(age);
-  print(cgpa);
-  print(isStudent);
+//   print(name);
+//   print(age);
+//   print(cgpa);
+//   print(isStudent);
+// }
+
+
+// String Interpolation
+
+// void main(){
+//   String name= "mahia";
+//   int age= 22;
+//   double cgpa= 3.84;
+//   bool isStudent= true;
+//   String university = "IUB";
+//   String department = "CSE";
+//   int semester = 11;
+
+//   print ("Name:$name");
+//   print ("Age:$age");
+//   print ("CGPA:$cgpa");
+//   print ("Student:$isStudent");
+//   print ("University:$university");
+//   print ("Department:$department");
+//   print ("Semester:$semester");
+
+// }
+
+
+// using Var
+// void main (){
+//   var name="mahia";
+//   var city = "Dhaka";
+//   var zipCode = 1704;
+//   var temperature = 34.5;
+//   print(name);
+//   print(city);
+//   print(zipCode);
+//   print(temperature);
+// }
+
+// Concept: final
+void main() {
+  final university = "Independent University ,Bangladesh";
+
+  print(university);
 }
