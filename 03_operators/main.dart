27b1b997e441fc7afc13582c5180e63a09d1621b
Jannatul Mahia <jannatulmahia8215@gmail.com>
2026-06-16 01:45:11 +0600

    Completed operators and comparison operators

---
+++ 03_operators/main.dart
@@ -0,0 +1,32 @@
+void main (){
+  int a = 10;
+  int b = 5;
+
+  print(a == b);      
+  print(a != b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+//   print("Addition:${a+b}");                
+//   print("Substraction:${a-b}");
+//   print("Multiplication: ${a * b}");
+//   print("Division: ${a / b}");
+//   print("Remainder: ${a % b}");
+ }
+
+// import 'dart:io';
+
+// void main() {
+//   print("Enter first number:");
+//   int num1 = int.parse(stdin.readLineSync()!);
+
+//   print("Enter second number:");
+//   int num2 = int.parse(stdin.readLineSync()!);
+
+//   print("Addition = ${num1 + num2}");
+//   print("Subtraction = ${num1 - num2}");
+//   print("Multiplication = ${num1 * num2}");
+//   print("Division = ${num1 / num2}");
+//   print("Remainder = ${num1 % num2}");
+// }
