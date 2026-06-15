a760755c8bbcec2ba21ec19756caa71419cd0088
Jannatul Mahia <jannatulmahia8215@gmail.com>
2026-06-16 01:55:50 +0600

    Completed conditional statements (if-else)

---
+++ 04_conditionals/main.dart
@@ -0,0 +1,39 @@
+// example 1
+
+
+// void main(){
+//   int age = 20;
+//   if (age >= 18){
+//     print("You are an adult");
+//   }
+// }
+
+
+
+// example 2
+
+// void main() {
+//   int age = 16;
+
+//   if (age >= 18) {
+//     print("Adult");
+//   } else {
+//     print("Not an adult");
+//   }
+// }
+
+// example 4
+
+void main() {
+  int marks = 75;
+
+  if (marks >= 80) {
+    print("A+");
+  } else if (marks >= 70) {
+    print("A");
+  } else if (marks >= 60) {
+    print("A-");
+  } else {
+    print("Fail");
+  }
+}
