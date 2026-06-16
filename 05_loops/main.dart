06a36e0d60905aacbda0ae1e8887d0493a1f89c4
Jannatul Mahia <jannatulmahia8215@gmail.com>
2026-06-17 00:37:27 +0600

    completed for loop

--- 05_loops/main.dart
+++ 05_loops/main.dart
@@ -1,5 +1,29 @@
-void main() {
-  for (int i = 1; i <= 5; i++) {
-    print(i);
+// void main() {
+//   for (int i = 1; i <= 5; i++) {
+//     print(i);
+//   }
+// }
+
+// void main() {
+//   for (int i = 1; i <= 5; i++) {
+//     print("hello");
+//   }
+// }
+
+// void main(){
+//   for(int i=2; i<=10 ;i+=2){
+//     print(i);
+//   }
+// }
+
+// void main() {
+//   for (int i = 1; i <= 10; i++) {
+//     print("5 x $i = ${5 * i}");
+//   }
+// }
+
+void main(){
+  for (int i=1; i<=10; i++){
+    print("7 x $i = ${7 * i}");
   }
 }
