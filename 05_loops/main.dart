bdc27f18c3b507fd27a22ba7443e30f2bf60310a
Jannatul Mahia <jannatulmahia8215@gmail.com>
2026-06-17 01:06:11 +0600

    added while loop and do-while loop

--- 05_loops/main.dart
+++ 05_loops/main.dart
@@ -22,8 +22,35 @@
 //   }
 // }
 
-void main(){
-  for (int i=1; i<=10; i++){
-    print("7 x $i = ${7 * i}");
-  }
+// void main(){
+//   for (int i=1; i<=10; i++){
+//     print("7 x $i = ${7 * i}");
+//   }
+// }
+
+// void main() {
+//   int i = 2;
+
+//   while (i <= 10) {
+//     print(i);
+//     i+= 2;
+//   }
+// }
+
+// void main() {
+//   int i = 1;
+
+//   while (i <= 10) {
+//     print("7 x $i = ${7 * i}");
+//     i++;
+//   }
+// }
+
+void main() {
+  int i = 1;
+
+  do {
+    print(i);
+    i++;
+  } while (i <= 5);
 }
