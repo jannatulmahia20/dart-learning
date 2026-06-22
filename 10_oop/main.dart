d5a42bc2f112657acd6560c998e9438b979049ec
Jannatul Mahia <jannatulmahia8215@gmail.com>
2026-06-23 00:56:36 +0600

    completed oop

--- 10_oop/main.dart
+++ 10_oop/main.dart
@@ -1,14 +1,53 @@
+// class Student {
+//   String name = "";
+//   int age = 0;
+// }
+
+// void main() {
+//   Student student1 = Student();
+
+//   student1.name = "Mahia";
+//   student1.age = 22;
+
+//   print(student1.name);
+//   print(student1.age);
+// }
+
+
+// class Student {
+//   String name = "";
+//   int age = 0;
+// }
+
+// void main() {
+//   Student student1 = Student();
+//   Student student2 = Student();
+
+//   student1.name = "Mahia";
+//   student1.age = 22;
+
+//   student2.name = "John";
+//   student2.age = 25;
+
+//   print(student1.name);
+//   print(student2.name);
+// }
+
+
 class Student {
-  String name = "";
-  int age = 0;
+  String name;
+  int age;
+
+  Student(this.name, this.age);
+
+  void introduce() {
+    print("My name is $name");
+    print("I am $age years old");
+  }
 }
 
 void main() {
-  Student student1 = Student();
+  Student student1 = Student("Mahia", 22);
 
-  student1.name = "Mahia";
-  student1.age = 22;
-
-  print(student1.name);
-  print(student1.age);
+  student1.introduce();
 }
